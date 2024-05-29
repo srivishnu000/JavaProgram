4f40c31c236c6d066c17215bf3167cc9f64c9740
srivishnu000 <vs6865905@gmail.com>
2024-05-29 11:59:52 +0530

    Add files via upload

---
+++ Employee1.java
@@ -0,0 +1,33 @@
+public class Employee1 {
+    private int empId;
+    private String empName;
+    private int empAge;
+    private double empSalary;
+
+    
+    public Employee() {
+        
+    }
+
+        public Employee(int empId, String empName, int empAge, double empSalary) {
+        this.empId = empId;
+        this.empName = empName;
+        this.empAge = empAge;
+        this.empSalary = empSalary;
+    }
+
+       public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+public void setEmpName()
+{
+this.empName=empName;
+}
+    public String getEmpName() 
+        return empName;
+}
