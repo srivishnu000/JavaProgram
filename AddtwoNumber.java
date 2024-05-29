67f24c684d11de0c36abec5f28da964e25a2505e
srivishnu000 <vs6865905@gmail.com>
2024-05-29 11:56:12 +0530

    Add files via upload

---
+++ AddtwoNumber.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class AddtwoNumber
+{
+ static int plusMethod(int x,int y)
+{
+return x+y;
+}
+public static void main(String args[]){
+System.out.println(plusMethod(5,8));
+}
+}
