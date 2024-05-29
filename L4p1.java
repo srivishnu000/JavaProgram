05ec2f714e9b81ede19d8a5fa7938a7ef8468b63
srivishnu000 <vs6865905@gmail.com>
2024-05-29 12:06:29 +0530

    Add files via upload

---
+++ L4p1.java
@@ -0,0 +1,18 @@
+ import java.util.*;
+class L4p1
+{
+	public static void main(String arg[])
+	{
+		Scanner obj=new Scanner(System.in);
+	        System.out.println("Enter one side lenght value:");
+                int length = obj.nextInt();
+int f = 4;
+int area = length*length;
+int per = f*length;
+ System.out.println(" ");
+System.out.println("The area of Square is:"+area);
+System.out.println("The perimeter of Square is:"+per);
+
+}
+
+}
