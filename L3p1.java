b690942e10e311b7dc3be205622a1736238faa14
srivishnu000 <vs6865905@gmail.com>
2024-05-29 12:04:41 +0530

    Add files via upload

---
+++ L3p1.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class L3p1
+{
+	public static void main(String arg[])
+	{
+byte by=123;
+short s=3434; 
+int i=12334564;
+long l=78884562728393939L;
+float f=3.566f;
+double d=5.9763738398393;
+boolean b=true;
+char c='A';
+System.out.println("Byte     :     "+by);
+System.out.println("Short    :     "+s);
+System.out.println("Int      :     "+i);
+System.out.println("Long     :     "+l);
+System.out.println("Float    :     "+f);
+System.out.println("Double   :     "+d);
+System.out.println("Boolean  :     "+b);
+System.out.println("Char     :     "+c);
+}
+}
+
+
