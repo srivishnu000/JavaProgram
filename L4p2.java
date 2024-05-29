ec9a2d1a6f27ca16c0706b284ab9ae634f727d3b
srivishnu000 <vs6865905@gmail.com>
2024-05-29 12:06:47 +0530

    Add files via upload

---
+++ L4p2.java
@@ -0,0 +1,13 @@
+public class L4p2
+ {
+    public static void main(String[] args) {
+        Animal animal1 = new Cat();
+        Animal animal2 = new Dog();
+        
+        animal1.eat();
+        animal1.behavior();
+        
+        animal2.eat();
+        animal2.behavior();
+    }
+}
